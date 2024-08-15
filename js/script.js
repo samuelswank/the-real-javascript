8b4df142ca33e796af4025618e24b12ac28c1b95
Samuel Swank <samuelswank@Samuels-MacBook-Pro.local>
2024-08-15 10:45:06 -0600

    Fixed changeState() logic problem

--- js/script.js
+++ js/script.js
@@ -10,51 +10,51 @@ let $shift = $('.shift');
 
 mapping = {
     // Row 1
-    Escape: { base: 'esc' },
+    Escape: { row: 1, base: 'esc' },
     // Row 2
-    Backquote: { base: String.fromCharCode(0xa9b3), shift: '~', text: true },
-    Digit1: { base: String.fromCharCode(0xa9d1), shift: '!', text: true },
-    Digit2: { base: String.fromCharCode(0xa9d2), shift: String.fromCharCode(0xa9cf), text: true },
-    Digit3: { base: String.fromCharCode(0xa9d3), shift: String.fromCharCode(0xa9c3), text: true },
-    Digit4: { base: String.fromCharCode(0xa9d4), shift: String.fromCharCode(0xa9c4), text: true },
-    Digit5: { base: String.fromCharCode(0xa9d5), shift: String.fromCharCode(0xa9c5), text: true },
-    Digit6: { base: String.fromCharCode(0xa9d6), shift: String.fromCharCode(0x2038), text: true },
-    Digit7: { base: String.fromCharCode(0xa9d7), shift: String.fromCharCode(0xa9cb), text: true },
-    Digit8: { base: String.fromCharCode(0xa9d8), shift: String.fromCharCode(0xa9de), text: true },
-    Digit9: { base: String.fromCharCode(0xa9d9), shift: String.fromCharCode(0xa9cc), text: true },
-    Digit0: { base: String.fromCharCode(0xa9d0), shift: String.fromCharCode(0xa9cd), text: true },
-    Minus: { base: String.fromCharCode(0xa9df), shift: '_', text: true },
-    Equal: { base: String.fromCharCode(0xa9c8), shift: String.fromCharCode(0xa9c9), text: true },
-    Backspace: { base: '⌫', text: false },
+    Backquote: { row: 2, base: String.fromCharCode(0xa9b3), shift: '~', text: true },
+    Digit1: { row: 2, base: String.fromCharCode(0xa9d1), shift: '!', text: true },
+    Digit2: { row: 2, base: String.fromCharCode(0xa9d2), shift: String.fromCharCode(0xa9cf), text: true },
+    Digit3: { row: 2, base: String.fromCharCode(0xa9d3), shift: String.fromCharCode(0xa9c3), text: true },
+    Digit4: { row: 2, base: String.fromCharCode(0xa9d4), shift: String.fromCharCode(0xa9c4), text: true },
+    Digit5: { row: 2, base: String.fromCharCode(0xa9d5), shift: String.fromCharCode(0xa9c5), text: true },
+    Digit6: { row: 2, base: String.fromCharCode(0xa9d6), shift: String.fromCharCode(0x2038), text: true },
+    Digit7: { row: 2, base: String.fromCharCode(0xa9d7), shift: String.fromCharCode(0xa9cb), text: true },
+    Digit8: { row: 2, base: String.fromCharCode(0xa9d8), shift: String.fromCharCode(0xa9de), text: true },
+    Digit9: { row: 2, base: String.fromCharCode(0xa9d9), shift: String.fromCharCode(0xa9cc), text: true },
+    Digit0: { row: 2, base: String.fromCharCode(0xa9d0), shift: String.fromCharCode(0xa9cd), text: true },
+    Minus: { row: 2, base: String.fromCharCode(0xa9df), shift: '_', text: true },
+    Equal: { row: 2, base: String.fromCharCode(0xa9c8), shift: String.fromCharCode(0xa9c9), text: true },
+    Backspace: { row: 2, base: '⌫', text: false },
     // Row 3
-    Tab: { base: '↹', text: false },
-    KeyQ: { base: String.fromCharCode(0xa9b2), shift: String.fromCharCode(0xa984), text: true },
-    KeyW: { base: String.fromCharCode(0xa9a4), shift: String.fromCharCode(0xa99f), text: true },
-    KeyE: { base: String.fromCharCode(0xa995), shift: String.fromCharCode(0xa996), text: true },
-    KeyR: { base: String.fromCharCode(0xa9ab), shift: String.fromCharCode(0xa9ac), text: true },
-    KeyT: { base: String.fromCharCode(0xa98f), shift: String.fromCharCode(0xa991), text: true },
-    KeyY: { base: String.fromCharCode(0xa9a2), shift: String.fromCharCode(0xa9a3), text: true },
-    KeyU: { base: String.fromCharCode(0xa9a0), shift: String.fromCharCode(0xa9a1), text: true },
-    KeyI: { base: String.fromCharCode(0xa9b1), shift: String.fromCharCode(0xa9af), text: true },
-    KeyO: { base: String.fromCharCode(0xa9ae), shift: String.fromCharCode(0xa9b0), text: true },
-    KeyP: { base: String.fromCharCode(0xa9ad), shift: String.fromCharCode(0xa985), text: true },
-    BracketLeft: { base: String.fromCharCode(0xa9c1), shift: '{', text: true },
-    BracketRight: { base: String.fromCharCode(0xa9c2), shift: '}', text: true },
-    Backslash: { base: String.fromCharCode(0xa9ca), shift: '|', text: true },
+    Tab: { row: 3, base: '↹', text: false },
+    KeyQ: { row: 3, base: String.fromCharCode(0xa9b2), shift: String.fromCharCode(0xa984), text: true },
+    KeyW: { row: 3, base: String.fromCharCode(0xa9a4), shift: String.fromCharCode(0xa99f), text: true },
+    KeyE: { row: 3, base: String.fromCharCode(0xa995), shift: String.fromCharCode(0xa996), text: true },
+    KeyR: { row: 3, base: String.fromCharCode(0xa9ab), shift: String.fromCharCode(0xa9ac), text: true },
+    KeyT: { row: 3, base: String.fromCharCode(0xa98f), shift: String.fromCharCode(0xa991), text: true },
+    KeyY: { row: 3, base: String.fromCharCode(0xa9a2), shift: String.fromCharCode(0xa9a3), text: true },
+    KeyU: { row: 3, base: String.fromCharCode(0xa9a0), shift: String.fromCharCode(0xa9a1), text: true },
+    KeyI: { row: 3, base: String.fromCharCode(0xa9b1), shift: String.fromCharCode(0xa9af), text: true },
+    KeyO: { row: 3, base: String.fromCharCode(0xa9ae), shift: String.fromCharCode(0xa9b0), text: true },
+    KeyP: { row: 3, base: String.fromCharCode(0xa9ad), shift: String.fromCharCode(0xa985), text: true },
+    BracketLeft: { row: 3, base: String.fromCharCode(0xa9c1), shift: '{', text: true },
+    BracketRight: { row: 3, base: String.fromCharCode(0xa9c2), shift: '}', text: true },
+    Backslash: { row: 3, base: String.fromCharCode(0xa9ca), shift: '|', text: true },
     // Row 4
     CapsLock: { base: '⇪', text: false },
-    KeyA: { base: String.fromCharCode(0xa9a5), shift: String.fromCharCode(0xa9a6), text: true },
-    KeyS: { base: String.fromCharCode(0xa99d), shift: String.fromCharCode(0xa99e), text: true },
-    KeyD: { base: String.fromCharCode(0xa997), shift: String.fromCharCode(0xa999), text: true },
-    KeyF: { base: String.fromCharCode(0xa9aa), shift: String.fromCharCode(0xa986), text: true },
-    KeyG: { base: String.fromCharCode(0xa99a), shift: String.fromCharCode(0xa998), text: true },
-    KeyH: { base: String.fromCharCode(0xa9a9), shift: String.fromCharCode(0xa987), text: true },
-    KeyJ: { base: String.fromCharCode(0xa992), shift: String.fromCharCode(0xa993), text: true },
-    KeyK: { base: String.fromCharCode(0xa9a7), shift: String.fromCharCode(0xa99c), text: true },
-    KeyL: { base: String.fromCharCode(0xa99b), shift: String.fromCharCode(0xa988), text: true },
-    Semicolon: { base: String.fromCharCode(0xa994), shift: String.fromCharCode(0xa9c7), text: true },
-    Quote: { base: String.fromCharCode(0xa9c0), shift: String.fromCharCode(0xa9be), text: true },
-    Enter: { base: '↵', text: false },
+    KeyA: { row: 4, base: String.fromCharCode(0xa9a5), shift: String.fromCharCode(0xa9a6), text: true },
+    KeyS: { row: 4, base: String.fromCharCode(0xa99d), shift: String.fromCharCode(0xa99e), text: true },
+    KeyD: { row: 4, base: String.fromCharCode(0xa997), shift: String.fromCharCode(0xa999), text: true },
+    KeyF: { row: 4, base: String.fromCharCode(0xa9aa), shift: String.fromCharCode(0xa986), text: true },
+    KeyG: { row: 4, base: String.fromCharCode(0xa99a), shift: String.fromCharCode(0xa998), text: true },
+    KeyH: { row: 4, base: String.fromCharCode(0xa9a9), shift: String.fromCharCode(0xa987), text: true },
+    KeyJ: { row: 4, base: String.fromCharCode(0xa992), shift: String.fromCharCode(0xa993), text: true },
+    KeyK: { row: 4, base: String.fromCharCode(0xa9a7), shift: String.fromCharCode(0xa99c), text: true },
+    KeyL: { row: 4, base: String.fromCharCode(0xa99b), shift: String.fromCharCode(0xa988), text: true },
+    Semicolon: { row: 4, base: String.fromCharCode(0xa994), shift: String.fromCharCode(0xa9c7), text: true },
+    Quote: { row: 4, base: String.fromCharCode(0xa9c0), shift: String.fromCharCode(0xa9be), text: true },
+    Enter: { row: 4, base: '↵', text: false },
     // Row 5
     ShiftLeft: { base: '⇧' },
     KeyZ : { base:  String.fromCharCode(0xa9b6), shift: String.fromCharCode(0xa9b7), text: true },
@@ -79,27 +79,38 @@ mapping = {
 
 $(window).on('load', () => {
     for (let i = 1; i < 7; ++i) {
-        generateKeys('.row-' + i, 'base');
+        generateKeys('.row-' + i, state);
     }
 });
 
 $editor.on('keydown', e => {
     keys[e.code] = true;
 
+    console.log(e.code);
+    console.log(keys);
+
     let $key = $('#' + e.code);
     $key.addClass('hover');
 
     let textToInsert;
 
-    if (typeof keys['ShiftLeft'] !== 'undefined' ||
-        typeof keys['ShiftRight'] !== 'undefined')
+    if (e.code === 'ShiftLeft' || e.code === 'ShiftRight')
         changeState();
+
+    console.log(state);
     
     if (mapping[e.code].text)
         textToInsert = $key.html();
 
+    console.log(state);
+
     if (e.key === 'Escape') {
         $editor.blur();
+        delete keys[e.code];
+        // Workaround, 'keyup' behavior was not working for 'Escape' key
+        setTimeout(() => {
+            $('#' + e.code).removeClass('hover');
+        }, 125);
     } else if (e.key === 'Backspace') {
         return;
     } else if (e.key === 'Tab') {
@@ -119,8 +130,7 @@ $editor.on('keyup', e => {
     let $key = $('#' + e.code);
     $key.removeClass('hover');
 
-    if (typeof keys['ShiftLeft'] !== 'undefined' ||
-        typeof keys['ShiftRight'] !== 'undefined')
+    if (e.code === 'ShiftLeft' || e.code === 'ShiftRight')
         changeState();
 
     delete keys[e.code];
